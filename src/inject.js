299546610ac1977fc7858536da4ab3db775e1145
Memedroid Eventos <123564826+Cristian-Nascimento-Mauricio@users.noreply.github.com>
2025-01-31 09:20:42 -0300

    add document of code

--- src/inject.js
+++ src/inject.js
@@ -1,10 +1,12 @@
+// Declaration of login page components
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const button = document.getElementById('button-login')
 
+// Input event to trigger login button
 const inputEvent = new Event('input', { bubbles: true });
 
-
+// Get values of stored 
 chrome.storage.local.get(["username", "password","checked"], (result) => {
     if (result.username === undefined || result.password === undefined ) 
         return
--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
+// Declaration of popup components
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const checkbox = document.getElementById('auto-login')
 
-
+// Verification of stored values to insert into the login page
 chrome.storage.local.get(["username", "password","checked"], (result) => {
     if (result.username === undefined || result.password === undefined ) 
         return
@@ -15,17 +16,19 @@ chrome.storage.local.get(["username", "password","checked"], (result) => {
 
 })
 
-
+// Save username 
 username.addEventListener('change',()=>{
     chrome.storage.local.set({ username: username.value });
 
 })
 
+// Save password
 password.addEventListener('change',()=>{
     chrome.storage.local.set({ password: password.value });
 
 })
 
+// Save cheeckbox status 
 checkbox.addEventListener('change',()=>{
     chrome.storage.local.set({checked:checkbox.checked})
 
