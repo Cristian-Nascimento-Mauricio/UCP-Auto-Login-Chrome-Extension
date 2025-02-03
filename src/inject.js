30d04e538cdc172554d6ed1bc3b9e5e73c8b4328
Memedroid Eventos <123564826+Cristian-Nascimento-Mauricio@users.noreply.github.com>
2025-02-03 11:41:13 -0300

    update logged in

--- src/inject.js
+++ src/inject.js
@@ -2,23 +2,45 @@
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const button = document.getElementById('button-login')
+const cookies = document.cookies
 
 // Input event to trigger login button
 const inputEvent = new Event('input', { bubbles: true });
 
-// Get values of stored 
-chrome.storage.local.get(["username", "password","checked"], (result) => {
-    if (result.username === undefined || result.password === undefined ) 
+run(username , password , button)
+
+function run(username , password , button){
+    // Check if you are already logged in
+    if(getCookie('user-data') != null)
         return
+
+    // Get values of stored 
+    chrome.storage.local.get(['username', 'password','checked'], (result) => {
+        if (result.username === undefined || result.password === undefined ) 
+            return
     
 
-    username.value = result.username;
-    username.dispatchEvent(inputEvent)
+        username.value = result.username;
+        username.dispatchEvent(inputEvent)
 
-    password.value = result.password;
-    password.dispatchEvent(inputEvent)
+        password.value = result.password;
+        password.dispatchEvent(inputEvent)
 
-    if(result.checked)
-        button.click()
+        if(result.checked)
+            button.click()
 
-});
+    });
+
+
+}
+
+function getCookie(nome) {
+    let cookies = document.cookie.split('; ');
+    for (let cookie of cookies) {
+        let [chave, valor] = cookie.split('=');
+        if (chave === nome) {
+            return decodeURIComponent(valor);
+        }
+    }
+    return null;
+}
